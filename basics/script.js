5cfa636077e65a8ce9a9e8cf04e4ad24a63e7a0e
Water <oboyiloejeh@gmail.com>
2024-10-12 04:58:31 +0000

    lesson 1 done

---
+++ basics/script.js
@@ -0,0 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+//functions
+function checkGuess() {
+    alert("I am a placeholder");
+  }
+  
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const { createServer } = require('node:http');
+const { readFile } = require('node:fs');
+const path = require('node:path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  const filePath = path.join(__dirname, 'basics', 'index.html');
+
+  readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Error loading the file.');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
