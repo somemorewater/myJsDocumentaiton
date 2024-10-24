9c90e241f3f8e9ddb24530e8530551e6f3e6fc89
somemorewater <oboyiloejeh@gmail.com>
2024-10-24 03:57:04 +0100

    ahh

--- basics/array/silly story generator/main.js
+++ basics/array/silly story generator/main.js
@@ -1,55 +1,56 @@
-const customName = document.getElementById("customname");
-const randomize = document.querySelector(".randomize");
-const story = document.querySelector(".story");
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
-function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-const storyText =
-  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
 const insertX = [
-                    "Willy the Goblin",
-                    "Big Daddy",
-                    "Father Christmas"
-                    ];
+  "Willy the Goblin",
+  "Big Daddy",
+  "Father Christmas"
+];
 
 const insertY = [
-                    "the soup kitchen",
-                    "Disneyland",
-                    "the White House"
-                ];
+  "the soup kitchen",
+  "Disneyland",
+  "the White House"
+];
 
 const insertZ = [
-                    "spontaneously combusted",
-                    "melted into a puddle on the sidewalk",
-                    "turned into a slug and crawled away"
-                ];
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away"
+
+]
 
 
 randomize.addEventListener('click', result);
 
 function result() {
 
-    const newStory = storyText;
+  if(customName.value !== '') {
+    const name = customName.value;
 
-    let xItem = randomValueFromArray(insertX);
-    let yItem = randomValueFromArray(insertY);
-    let zItem = randomValueFromArray(insertZ);
+  }
 
-    if(customName.value !== '') {
-        const name = customName.value;
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
 
-    }
+  }
 
-    if(document.getElementById("uk").checked) {
-        const weight = Math.round(300);
-        const temperature =  Math.round(94);
-
-    }
-
-    story.textContent = [];
-    story.style.visibility = 'visible';
+  //story.textContent = ;
+  story.style.visibility = 'visible';
 }
+
+let newStory = storyText;
+
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
