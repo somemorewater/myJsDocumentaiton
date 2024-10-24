77fa167a73a4abebb857245efe97fc5386256ca7
somemorewater <oboyiloejeh@gmail.com>
2024-10-24 04:43:04 +0100

    move up today

---
+++ js building blocks/conditionals/script.js
@@ -0,0 +1,23 @@
+//Basic if...else syntax
+
+if (condition) {
+    //code to run
+} else {
+    //code to run instead
+}
+
+//A real example
+
+let shoppingDone = false;
+let childsAllowance;
+
+if (shoppingDone === true) {
+  childsAllowance = 10;
+} else {
+  childsAllowance = 5;
+}
+
+/*
+we use else if to add extra  
+posibilities
+ */
