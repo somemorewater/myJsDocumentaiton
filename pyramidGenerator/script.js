1fa17eb500c11c7d0320146bfa42e2fb2b2e6ed2
somemorewater <oboyiloejeh@gmail.com>
2025-01-13 03:43:03 +0100

    done with for loop want to try another

--- pyramidGenerator/script.js
+++ pyramidGenerator/script.js
@@ -6,9 +6,10 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i++) {
-    rows.push(padRow(i + 1, count));
-}
+//TODO: use a different king of loop
+/*for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+}*/
 
 let result = "";
 
