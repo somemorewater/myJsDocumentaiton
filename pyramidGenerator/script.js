df59684b86756a74ee148868fbd3393f02999baa
somemorewater <oboyiloejeh@gmail.com>
2025-01-13 02:06:02 +0100

    making progress slowly

--- pyramidGenerator/script.js
+++ pyramidGenerator/script.js
@@ -1,3 +1,16 @@
 const character = "#";
 const count = 8;
 const rows = [];
+
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character);
+}
+
+let result = "";
+
+for (const row of rows) {
+    result = result + row + "\n";
+}
+
+console.log(result);
