449fa50444d54126a2eb63da9cf1b3383e78c23f
somemorewater <oboyiloejeh@gmail.com>
2025-01-15 03:56:10 +0100

    done witth it

--- pyramidGenerator/script.js
+++ pyramidGenerator/script.js
@@ -1,28 +1,30 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 //TODO: use a different king of loop
-/*for (let i = 1; i <= count; i++) {
-    rows.push(padRow(i, count));
-}*/
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    } else {
+        rows.push(padRow(i, count));
+    }
+}
 
- 
+//Other methods 
 /*while (rows.length < count) {
     rows.push(padRow(rows.length + 1, count));
-}*/
+}
 
-/*for (let i = count; i > 1; i--) {
+for (let i = count; i > 1; i--) {
     rows.push(padRow(i, count));
 }*/
 
-const numbers = [1, 2, 3];
-const shifted = numbers.shift();
-const unshifted = numbers.unshift(5)
 
 let result = "";
 
