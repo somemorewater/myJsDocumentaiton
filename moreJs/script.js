5b9cffa077fecdcd1720ed23680101fb28a3d78d
somemorewater <oboyiloejeh@gmail.com>
2025-01-18 19:33:34 +0100

    add contacts address book and display functionality

---
+++ moreJs/script.js
@@ -0,0 +1,16 @@
+const contacts = {
+  addressBook: [
+    { name: "Michael Olmo", email: "michaelolmo@gmail.com" },
+    { name: "Jordan Philip", email: "jordanphilip@gmail.com" },
+    { name: "Grace Finn", email: "gracefinn@gmail.com" },
+    { name: "Paul Seed", email: "paulseed@gmail.com" },
+    { name: "John Doe", email: "johndoe@gmail.com" },
+  ],
+};
+
+const object = contacts.addressBook,
+  contactsCount = object.length;
+
+for (let i = 0; i < contactsCount; i++) {
+  console.log(object[i]);
+}
