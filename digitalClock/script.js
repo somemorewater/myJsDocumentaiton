ae2b45620ab85540a9488cc5984f0964a222c309
Water <oboyiloejeh@gmail.com>
2026-01-30 20:34:29 +0100

    Update script.js

--- digitalClock/script.js
+++ digitalClock/script.js
@@ -3,7 +3,6 @@ function time() {
     let hour = now.getHours();
     let minute = now.getMinutes();
     let second = now.getSeconds();
-    // console.log(`${hour}:${minute}:${second}`);
     document.querySelector(
       "#digitalClock"
     ).innerHTML = `${hour}:${minute}:${second}`;
